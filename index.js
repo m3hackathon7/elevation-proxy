1017d5ee87a5cd3d20ebc0dde3396a5447677db4
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-10-13 18:05:59 +0900

    Initial commit.

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var request = require('request');
+
+var port = process.env['PORT'] || 3333;
+
+var app = express();
+app.get('/routes', createProxyRoute('http://maps.googleapis.com/maps/api/directions/json'));
+app.get('/elevations', createProxyRoute('https://maps.googleapis.com/maps/api/elevation/json'));
+
+app.listen(port, function() {
+  console.error('Listening on', port);
+});
+
+function createProxyRoute(url) {
+  return function(req, res) {
+    var params = { url: url, qs: req.query };
+    res.writeHead(200, corsHeader());
+    request.get(params).pipe(res);
+  };
+}
+
+function corsHeader() {
+  return {
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    'Access-Control-Allow-Origin': '*'
+  };
+}
